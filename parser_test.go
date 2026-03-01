91e3b189bc0da9c9597dedbf867e94c5ae4264e7
Mark Ferrell <major@homeonderanged.org>
2026-03-01 10:57:59 -0800

    test(optargs): clean up parser_test.go
    Extract shared helpers to reduce duplication: graphChars replaces genOpts,
    argTypeForIndex replaces repeated switch-on-modulo blocks, and newTestParser
    replaces four identical short/long option map declarations.
    
    Convert TestParserInitInvalidShortOpts to table-driven with subtests.
    Convert TestParserLongOptsProvidedArgs to table-driven with subtests.
    Remove duplicated assertion in TestParserPosixNonOpt.
    Use consistent error formatting (%v) and assertion messages throughout.
    Remove unnecessary composite literal field names (&Flag{...}).

--- parser_test.go
+++ parser_test.go
@@ -4,332 +4,311 @@ import (
 	"testing"
 )
 
-func genOpts() []byte {
-	var opts []byte
+// graphChars returns every byte value for which isGraph reports true.
+var graphChars = func() []byte {
+	var out []byte
 	for i := 0; i < 255; i++ {
-		if !isGraph(byte(i)) {
-			continue
+		if isGraph(byte(i)) {
+			out = append(out, byte(i))
 		}
-		opts = append(opts, byte(i))
 	}
-	return opts
+	return out
+}()
+
+// argTypeForIndex cycles through NoArgument, RequiredArgument,
+// OptionalArgument based on the byte value. Used by init tests to
+// exercise all three argument types without duplicating the switch.
+func argTypeForIndex(b byte) ArgType {
+	switch b % 3 {
+	case 1:
+		return RequiredArgument
+	case 2:
+		return OptionalArgument
+	default:
+		return NoArgument
+	}
+}
+
+// newTestParser creates a parser with a single short option 'a' (NoArgument)
+// and a single long option "a" (NoArgument). Reduces boilerplate in tests
+// that only need a minimal option set.
+func newTestParser(t *testing.T, config ParserConfig, args []string) *Parser {
+	t.Helper()
+	shopts := map[byte]*Flag{
+		'a': {Name: "a", HasArg: NoArgument},
+	}
+	lopts := map[string]*Flag{
+		"a": {Name: "a", HasArg: NoArgument},
+	}
+	p, err := NewParser(config, shopts, lopts, args)
+	if err != nil {
+		t.Fatalf("NewParser: %v", err)
+	}
+	return p
 }
 
 func TestParserInit(t *testing.T) {
 	_, err := NewParser(ParserConfig{}, nil, nil, nil)
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
+		t.Errorf("NewParser: %v", err)
 	}
 }
 
 func TestParserInitShortOpts(t *testing.T) {
-	var shortOpts = make(map[byte]*Flag)
-	for _, i := range genOpts() {
-		switch i {
+	shortOpts := make(map[byte]*Flag)
+	for _, c := range graphChars {
+		switch c {
 		case ':', ';', '-':
 			continue
 		}
-
-		var hasArg ArgType
-		switch i % 3 {
-		case 0:
-			hasArg = NoArgument
-		case 1:
-			hasArg = RequiredArgument
-		case 2:
-			hasArg = OptionalArgument
-		}
-
-		shortOpts[i] = &Flag{Name: string(i), HasArg: hasArg}
+		shortOpts[c] = &Flag{Name: string(c), HasArg: argTypeForIndex(c)}
 	}
 	_, err := NewParser(ParserConfig{}, shortOpts, nil, nil)
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
+		t.Errorf("NewParser: %v", err)
 	}
 }
-func TestParserInitInvalidShortOpts(t *testing.T) {
-	var shortOpts = map[byte]*Flag{
-		':': &Flag{Name: ":", HasArg: NoArgument},
-		';': &Flag{Name: ";", HasArg: NoArgument},
-		'-': &Flag{Name: "-", HasArg: NoArgument},
-	}
 
-	_, err := NewParser(ParserConfig{}, shortOpts, nil, nil)
-	if err == nil {
-		t.Errorf("Expected invalid option error")
+var invalidShortOptTests = []struct {
+	name string
+	key  byte
+}{
+	{name: "colon", key: ':'},
+	{name: "semicolon", key: ';'},
+	{name: "dash", key: '-'},
+}
+
+func TestParserInitInvalidShortOpts(t *testing.T) {
+	for _, tt := range invalidShortOptTests {
+		t.Run(tt.name, func(t *testing.T) {
+			shortOpts := map[byte]*Flag{
+				tt.key: {Name: string(tt.key), HasArg: NoArgument},
+			}
+			_, err := NewParser(ParserConfig{}, shortOpts, nil, nil)
+			if err == nil {
+				t.Errorf("expected error for short option %q", tt.key)
+			}
+		})
 	}
 }
 
 func TestParserInitNotIsGraphShortOpts(t *testing.T) {
-	var shortOpts = map[byte]*Flag{
-		' ': &Flag{Name: " ", HasArg: NoArgument},
+	shortOpts := map[byte]*Flag{
+		' ': {Name: " ", HasArg: NoArgument},
 	}
-
 	_, err := NewParser(ParserConfig{}, shortOpts, nil, nil)
 	if err == nil {
-		t.Errorf("Expected invalid option error")
+		t.Error("expected error for non-graphic short option")
 	}
 }
 
 func TestParserInitNotIsGraphLongOpts(t *testing.T) {
-	var options = map[string]*Flag{
-		" ": &Flag{Name: " ", HasArg: NoArgument},
+	longOpts := map[string]*Flag{
+		" ": {Name: " ", HasArg: NoArgument},
 	}
-
-	_, err := NewParser(ParserConfig{}, nil, options, nil)
+	_, err := NewParser(ParserConfig{}, nil, longOpts, nil)
 	if err == nil {
-		t.Errorf("Expected invalid option error")
+		t.Error("expected error for non-graphic long option")
 	}
 }
 
 func TestParserInitLongOpts(t *testing.T) {
-	var longOpts = make(map[string]*Flag)
-	for _, i := range genOpts() {
-		s := string(rune(i))
-		var hasArg ArgType
-		switch i % 3 {
-		case 0:
-			hasArg = NoArgument
-		case 1:
-			hasArg = RequiredArgument
-		case 2:
-			hasArg = OptionalArgument
-		}
-
-		longOpts[s] = &Flag{Name: s, HasArg: hasArg}
+	longOpts := make(map[string]*Flag)
+	for _, c := range graphChars {
+		s := string(rune(c))
+		longOpts[s] = &Flag{Name: s, HasArg: argTypeForIndex(c)}
 	}
 	_, err := NewParser(ParserConfig{}, nil, longOpts, nil)
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
+		t.Errorf("NewParser: %v", err)
 	}
 }
 
 func TestParserPosixBreak(t *testing.T) {
-	shopts := map[byte]*Flag{
-		'a': &Flag{Name: "a", HasArg: NoArgument},
-	}
-	lopts := map[string]*Flag{
-		"a": &Flag{Name: "a", HasArg: NoArgument},
-	}
-	args := []string{"--", "-a", "--a"}
-
-	parser, err := NewParser(ParserConfig{}, shopts, lopts, args)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
+	parser := newTestParser(t, ParserConfig{}, []string{"--", "-a", "--a"})
 
 	for opt, err := range parser.Options() {
 		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
+			t.Errorf("Options: %v", err)
 		}
-		t.Errorf("Unexpected option %s", opt.Name)
+		t.Errorf("unexpected option %s", opt.Name)
 	}
 
 	if parser.Args[0] != "-a" {
-		t.Errorf("Unexpected argument %s", parser.Args[0])
+		t.Errorf("Args[0] = %q, want %q", parser.Args[0], "-a")
 	}
 }
 
 func TestParserNonOptShift(t *testing.T) {
-	shopts := map[byte]*Flag{
-		'a': &Flag{Name: "a", HasArg: NoArgument},
-	}
-	lopts := map[string]*Flag{
-		"a": &Flag{Name: "a", HasArg: NoArgument},
-	}
-	args := []string{"param", "-a", "--a"}
-
-	parser, err := NewParser(ParserConfig{}, shopts, lopts, args)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
+	parser := newTestParser(t, ParserConfig{}, []string{"param", "-a", "--a"})
 
 	for opt, err := range parser.Options() {
 		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
+			t.Errorf("Options: %v", err)
 		}
-
 		if opt.Name != "a" {
-			t.Errorf("Unexpected option %s", opt.Name)
+			t.Errorf("option = %q, want %q", opt.Name, "a")
 		}
 	}
 
 	if parser.Args[0] != "param" {
-		t.Errorf("Unexpected argument %s", parser.Args[0])
+		t.Errorf("Args[0] = %q, want %q", parser.Args[0], "param")
 	}
 }
 
 func TestParserNonOpt(t *testing.T) {
-	shopts := map[byte]*Flag{
-		'a': &Flag{Name: "a", HasArg: NoArgument},
-	}
-	lopts := map[string]*Flag{
-		"a": &Flag{Name: "a", HasArg: NoArgument},
-	}
-	args := []string{"-a", "param", "--a"}
-
-	parser, err := NewParser(ParserConfig{parseMode: ParseNonOpts}, shopts, lopts, args)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
+	parser := newTestParser(t, ParserConfig{parseMode: ParseNonOpts}, []string{"-a", "param", "--a"})
 
 	for opt, err := range parser.Options() {
 		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
+			t.Errorf("Options: %v", err)
 		}
-
 		switch opt.Name {
 		case "a":
-			// Expected option, no action needed
-
+			// expected
 		case string(byte(1)):
 			if opt.Arg != "param" {
-				t.Errorf("Unexpected argument %s", opt.Arg)
+				t.Errorf("non-opt arg = %q, want %q", opt.Arg, "param")
 			}
-
 		default:
-			t.Errorf("Unexpected option '%s'", opt.Name)
+			t.Errorf("unexpected option %q", opt.Name)
 		}
 	}
 
 	if len(parser.Args) != 0 {
-		t.Errorf("Unexpected argument %s", parser.Args[0])
+		t.Errorf("len(Args) = %d, want 0", len(parser.Args))
 	}
 }
 
 func TestParserPosixNonOpt(t *testing.T) {
-	shopts := map[byte]*Flag{
-		'a': &Flag{Name: "a", HasArg: NoArgument},
-	}
-	lopts := map[string]*Flag{
-		"a": &Flag{Name: "a", HasArg: NoArgument},
-	}
-	args := []string{"param", "-a", "--a"}
-
-	parser, err := NewParser(ParserConfig{parseMode: ParsePosixlyCorrect}, shopts, lopts, args)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
+	parser := newTestParser(t, ParserConfig{parseMode: ParsePosixlyCorrect}, []string{"param", "-a", "--a"})
 
 	for opt, err := range parser.Options() {
 		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
+			t.Errorf("Options: %v", err)
 		}
-		t.Errorf("Unexpected option %s", opt.Name)
+		t.Errorf("unexpected option %s", opt.Name)
 	}
 
 	if parser.Args[0] != "param" {
-		t.Errorf("Unexpected argument %s", parser.Args[0])
+		t.Errorf("Args[0] = %q, want %q", parser.Args[0], "param")
 	}
-
 	if parser.Args[1] != "-a" {
-		t.Errorf("Unexpected argument %s", parser.Args[1])
-	}
-
-	if parser.Args[1] != "-a" {
-		t.Errorf("Unexpected argument %s", parser.Args[1])
+		t.Errorf("Args[1] = %q, want %q", parser.Args[1], "-a")
 	}
 }
 
 func TestParserLongOptsLongPrefix(t *testing.T) {
-	var longOpts = map[string]*Flag{
-		"foobar": &Flag{Name: "foobar", HasArg: RequiredArgument},
+	longOpts := map[string]*Flag{
+		"foobar": {Name: "foobar", HasArg: RequiredArgument},
 	}
-
-	args := []string{"--foo"}
-	parser, err := NewParser(ParserConfig{}, nil, longOpts, args)
+	parser, err := NewParser(ParserConfig{}, nil, longOpts, []string{"--foo"})
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
+		t.Fatalf("NewParser: %v", err)
 	}
 	for _, err := range parser.Options() {
 		if err == nil {
-			t.Errorf("Expected unknown option for --foo")
+			t.Error("expected error for ambiguous prefix --foo")
 		}
 	}
+}
+
+var longOptPrefixTests = []struct {
+	name    string
+	arg     string
+	wantOpt string
+	wantArg bool
+}{
+	{name: "exact match with equals", arg: "--foo=", wantOpt: "foo=", wantArg: false},
 }
 
 func TestParserLongOptsPrefix(t *testing.T) {
-	var longOpts = map[string]*Flag{
-		"foo":  &Flag{Name: "foo", HasArg: RequiredArgument},
-		"foo=": &Flag{Name: "foo=", HasArg: NoArgument},
+	longOpts := map[string]*Flag{
+		"foo":  {Name: "foo", HasArg: RequiredArgument},
+		"foo=": {Name: "foo=", HasArg: NoArgument},
 	}
 
-	args := []string{"--foo="}
-	parser, err := NewParser(ParserConfig{}, nil, longOpts, args)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
-
-	count := 0
-	for opt, err := range parser.Options() {
-		count++
-		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
-		}
-		if opt.Name != "foo=" {
-			t.Errorf("Expected option %s, got %s", "foo=", opt.Name)
-		}
-		if opt.HasArg {
-			t.Errorf("Expected HasArg to be false")
-		}
-	}
-
-	if count != 1 {
-		t.Errorf("Expected 1 options, got %d", count)
+	for _, tt := range longOptPrefixTests {
+		t.Run(tt.name, func(t *testing.T) {
+			parser, err := NewParser(ParserConfig{}, nil, longOpts, []string{tt.arg})
+			if err != nil {
+				t.Fatalf("NewParser: %v", err)
+			}
+			count := 0
+			for opt, err := range parser.Options() {
+				count++
+				if err != nil {
+					t.Errorf("Options: %v", err)
+				}
+				if opt.Name != tt.wantOpt {
+					t.Errorf("option = %q, want %q", opt.Name, tt.wantOpt)
+				}
+				if opt.HasArg != tt.wantArg {
+					t.Errorf("HasArg = %v, want %v", opt.HasArg, tt.wantArg)
+				}
+			}
+			if count != 1 {
+				t.Errorf("option count = %d, want 1", count)
+			}
+		})
 	}
 }
 
+var longOptArgTests = []struct {
+	name    string
+	args    []string
+	wantArg bool
+}{
+	{name: "required with equals", args: []string{"--foo=bar"}, wantArg: true},
+	{name: "required with space", args: []string{"--foo", "bar"}, wantArg: true},
+	{name: "optional with equals", args: []string{"--boo=baz"}, wantArg: true},
+	{name: "optional with space", args: []string{"--boo", "baz"}, wantArg: true},
+}
+
 func TestParserLongOptsProvidedArgs(t *testing.T) {
-	var longOpts = map[string]*Flag{
-		"foo": &Flag{Name: "foo", HasArg: RequiredArgument},
-		"boo": &Flag{Name: "boo", HasArg: OptionalArgument},
+	longOpts := map[string]*Flag{
+		"foo": {Name: "foo", HasArg: RequiredArgument},
+		"boo": {Name: "boo", HasArg: OptionalArgument},
 	}
 
-	args := []string{"--foo=bar", "--foo", "bar", "--boo=baz", "--boo", "baz"}
-	parser, err := NewParser(ParserConfig{}, nil, longOpts, args)
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-	}
-
-	count := 0
-	for opt, err := range parser.Options() {
-		count++
-		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
-		}
-		if !opt.HasArg {
-			t.Errorf("Expected HasArg to be true")
-		}
-	}
-
-	if count != 4 {
-		t.Errorf("Expected 4 options, got %d", count)
+	for _, tt := range longOptArgTests {
+		t.Run(tt.name, func(t *testing.T) {
+			parser, err := NewParser(ParserConfig{}, nil, longOpts, tt.args)
+			if err != nil {
+				t.Fatalf("NewParser: %v", err)
+			}
+			for opt, err := range parser.Options() {
+				if err != nil {
+					t.Errorf("Options: %v", err)
+				}
+				if opt.HasArg != tt.wantArg {
+					t.Errorf("HasArg = %v, want %v", opt.HasArg, tt.wantArg)
+				}
+			}
+		})
 	}
 }
 
 func TestParserLongOptsMissingOptArg(t *testing.T) {
-	var longOpts = map[string]*Flag{
-		"foo": &Flag{Name: "foo", HasArg: OptionalArgument},
+	longOpts := map[string]*Flag{
+		"foo": {Name: "foo", HasArg: OptionalArgument},
 	}
-
-	args := []string{"--foo"}
-	parser, err := NewParser(ParserConfig{}, nil, longOpts, args)
+	parser, err := NewParser(ParserConfig{}, nil, longOpts, []string{"--foo"})
 	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
+		t.Fatalf("NewParser: %v", err)
 	}
 
 	count := 0
 	for opt, err := range parser.Options() {
 		count++
 		if err != nil {
-			t.Errorf("Unexpected error: %s", err)
+			t.Errorf("Options: %v", err)
 		}
 		if opt.HasArg {
-			t.Errorf("Expected HasArg to be false")
+			t.Error("HasArg = true, want false")
 		}
 	}
-
 	if count != 1 {
-		t.Errorf("Expected 2 options, got %d", count)
+		t.Errorf("option count = %d, want 1", count)
 	}
 }
