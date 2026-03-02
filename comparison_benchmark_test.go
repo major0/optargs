0caf47a485f68490860c94145e79eea974fb2cdf
Mark Ferrell <major@homeonderanged.org>
2026-03-01 16:31:28 -0800

    test(optargs): deduplicate comparison benchmark patterns
    Extract benchStdFlag helper to mirror benchParse for standard library
    benchmarks. Replace all inline OptArgs parse-and-iterate loops with
    calls to the existing benchParse helper. Replace fmt.Sprintf with
    strconv.Itoa for integer formatting. Use io.Discard instead of
    os.Stderr for suppressing flag.FlagSet output.

--- comparison_benchmark_test.go
+++ comparison_benchmark_test.go
@@ -3,50 +3,43 @@ package optargs
 import (
 	"flag"
 	"fmt"
-	"os"
+	"io"
+	"strconv"
 	"testing"
 )
 
+// benchStdFlag creates a flag.FlagSet, registers flags via setup, parses
+// args, and accesses all values.  Mirrors benchParse for the standard
+// library side of comparison benchmarks.
+func benchStdFlag(b *testing.B, args []string, setup func(*flag.FlagSet)) {
+	b.Helper()
+	fs := flag.NewFlagSet("test", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+	setup(fs)
+	if err := fs.Parse(args); err != nil {
+		b.Fatal(err)
+	}
+}
+
 // BenchmarkComparisonWithStdFlag compares performance with Go's standard flag package
 func BenchmarkComparisonWithStdFlag(b *testing.B) {
-	// Test case: simple flags with arguments
 	args := []string{"-a", "arg1", "-b", "arg2", "-c"}
 
 	b.Run("OptArgs", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			parser, err := GetOpt(append([]string{"prog"}, args...), "a:b:c")
-			if err != nil {
-				b.Fatal(err)
-			}
-			for option, err := range parser.Options() {
-				if err != nil {
-					b.Fatal(err)
-				}
-				_ = option
-			}
+			benchParse(b, append([]string{"prog"}, args...), "a:b:c", nil)
 		}
 	})
 
 	b.Run("StdFlag", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			fs := flag.NewFlagSet("test", flag.ContinueOnError)
-			fs.SetOutput(os.Stderr) // Suppress output during benchmarking
-
-			aFlag := fs.String("a", "", "a flag")
-			bFlag := fs.String("b", "", "b flag")
-			cFlag := fs.Bool("c", false, "c flag")
-
-			err := fs.Parse(args)
-			if err != nil {
-				b.Fatal(err)
-			}
-
-			// Access the values to ensure they're processed
-			_ = *aFlag
-			_ = *bFlag
-			_ = *cFlag
+			benchStdFlag(b, args, func(fs *flag.FlagSet) {
+				_ = fs.String("a", "", "a flag")
+				_ = fs.String("b", "", "b flag")
+				_ = fs.Bool("c", false, "c flag")
+			})
 		}
 	})
 }
@@ -63,45 +56,24 @@ func BenchmarkComparisonLongOptions(b *testing.B) {
 	b.Run("OptArgs", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			parser, err := GetOptLong(append([]string{"prog"}, args...), "", longOpts)
-			if err != nil {
-				b.Fatal(err)
-			}
-			for option, err := range parser.Options() {
-				if err != nil {
-					b.Fatal(err)
-				}
-				_ = option
-			}
+			benchParse(b, append([]string{"prog"}, args...), "", longOpts)
 		}
 	})
 
 	b.Run("StdFlag", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			fs := flag.NewFlagSet("test", flag.ContinueOnError)
-			fs.SetOutput(os.Stderr) // Suppress output during benchmarking
-
-			verboseFlag := fs.Bool("verbose", false, "verbose flag")
-			outputFlag := fs.String("output", "", "output flag")
-			configFlag := fs.String("config", "", "config flag")
-
-			err := fs.Parse(args)
-			if err != nil {
-				b.Fatal(err)
-			}
-
-			// Access the values to ensure they're processed
-			_ = *verboseFlag
-			_ = *outputFlag
-			_ = *configFlag
+			benchStdFlag(b, args, func(fs *flag.FlagSet) {
+				_ = fs.Bool("verbose", false, "verbose flag")
+				_ = fs.String("output", "", "output flag")
+				_ = fs.String("config", "", "config flag")
+			})
 		}
 	})
 }
 
 // BenchmarkComparisonComplexScenarios compares complex parsing scenarios
 func BenchmarkComparisonComplexScenarios(b *testing.B) {
-	// Complex scenario with mixed short/long options, compaction, etc.
 	args := []string{"-abc", "arg1", "--verbose", "--output=file.txt", "-d", "arg2"}
 	longOpts := []Flag{
 		{Name: "verbose", HasArg: NoArgument},
@@ -111,16 +83,7 @@ func BenchmarkComparisonComplexScenarios(b *testing.B) {
 	b.Run("OptArgs", func(b *testing.B) {
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			parser, err := GetOptLong(append([]string{"prog"}, args...), "abc:d:", longOpts)
-			if err != nil {
-				b.Fatal(err)
-			}
-			for option, err := range parser.Options() {
-				if err != nil {
-					b.Fatal(err)
-				}
-				_ = option
-			}
+			benchParse(b, append([]string{"prog"}, args...), "abc:d:", longOpts)
 		}
 	})
 
@@ -130,28 +93,14 @@ func BenchmarkComparisonComplexScenarios(b *testing.B) {
 
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			fs := flag.NewFlagSet("test", flag.ContinueOnError)
-			fs.SetOutput(os.Stderr) // Suppress output during benchmarking
-
-			aFlag := fs.Bool("a", false, "a flag")
-			bFlag := fs.Bool("b", false, "b flag")
-			cFlag := fs.String("c", "", "c flag")
-			verboseFlag := fs.Bool("verbose", false, "verbose flag")
-			outputFlag := fs.String("output", "", "output flag")
-			dFlag := fs.String("d", "", "d flag")
-
-			err := fs.Parse(equivalentArgs)
-			if err != nil {
-				b.Fatal(err)
-			}
-
-			// Access the values to ensure they're processed
-			_ = *aFlag
-			_ = *bFlag
-			_ = *cFlag
-			_ = *verboseFlag
-			_ = *outputFlag
-			_ = *dFlag
+			benchStdFlag(b, equivalentArgs, func(fs *flag.FlagSet) {
+				_ = fs.Bool("a", false, "a flag")
+				_ = fs.Bool("b", false, "b flag")
+				_ = fs.String("c", "", "c flag")
+				_ = fs.Bool("verbose", false, "verbose flag")
+				_ = fs.String("output", "", "output flag")
+				_ = fs.String("d", "", "d flag")
+			})
 		}
 	})
 }
@@ -167,16 +116,7 @@ func BenchmarkComparisonMemoryUsage(b *testing.B) {
 		b.ReportAllocs()
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			parser, err := GetOptLong(append([]string{"prog"}, args...), "a:b:", longOpts)
-			if err != nil {
-				b.Fatal(err)
-			}
-			for option, err := range parser.Options() {
-				if err != nil {
-					b.Fatal(err)
-				}
-				_ = option
-			}
+			benchParse(b, append([]string{"prog"}, args...), "a:b:", longOpts)
 		}
 	})
 
@@ -184,22 +124,11 @@ func BenchmarkComparisonMemoryUsage(b *testing.B) {
 		b.ReportAllocs()
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			fs := flag.NewFlagSet("test", flag.ContinueOnError)
-			fs.SetOutput(os.Stderr) // Suppress output during benchmarking
-
-			aFlag := fs.String("a", "", "a flag")
-			bFlag := fs.String("b", "", "b flag")
-			verboseFlag := fs.Bool("verbose", false, "verbose flag")
-
-			err := fs.Parse(args)
-			if err != nil {
-				b.Fatal(err)
-			}
-
-			// Access the values to ensure they're processed
-			_ = *aFlag
-			_ = *bFlag
-			_ = *verboseFlag
+			benchStdFlag(b, args, func(fs *flag.FlagSet) {
+				_ = fs.String("a", "", "a flag")
+				_ = fs.String("b", "", "b flag")
+				_ = fs.Bool("verbose", false, "verbose flag")
+			})
 		}
 	})
 }
@@ -210,54 +139,35 @@ func BenchmarkScalability(b *testing.B) {
 
 	for _, size := range sizes {
 		b.Run(fmt.Sprintf("OptArgs_Size%d", size), func(b *testing.B) {
-			// Generate args
 			args := make([]string, 0, size+1)
 			args = append(args, "prog")
-			for i := 1; i <= size; i++ {
-				if i%3 == 0 {
-					args = append(args, "-a", "arg"+fmt.Sprintf("%d", i))
+			for j := 1; j <= size; j++ {
+				if j%3 == 0 {
+					args = append(args, "-a", "arg"+strconv.Itoa(j))
 				} else {
-					args = append(args, "arg"+fmt.Sprintf("%d", i))
+					args = append(args, "arg"+strconv.Itoa(j))
 				}
 			}
 
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
-				parser, err := GetOpt(args, "a:")
-				if err != nil {
-					b.Fatal(err)
-				}
-				for option, err := range parser.Options() {
-					if err != nil {
-						b.Fatal(err)
-					}
-					_ = option
-				}
+				benchParse(b, args, "a:", nil)
 			}
 		})
 
 		b.Run(fmt.Sprintf("StdFlag_Size%d", size), func(b *testing.B) {
-			// Generate args for standard flag (no compaction)
 			args := make([]string, 0, size)
-			for i := 1; i <= size; i++ {
-				if i%2 == 0 {
-					args = append(args, "-a", "arg"+fmt.Sprintf("%d", i))
+			for j := 1; j <= size; j++ {
+				if j%2 == 0 {
+					args = append(args, "-a", "arg"+strconv.Itoa(j))
 				}
 			}
 
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
-				fs := flag.NewFlagSet("test", flag.ContinueOnError)
-				fs.SetOutput(os.Stderr) // Suppress output during benchmarking
-
-				aFlag := fs.String("a", "", "a flag")
-
-				err := fs.Parse(args)
-				if err != nil {
-					b.Fatal(err)
-				}
-
-				_ = *aFlag
+				benchStdFlag(b, args, func(fs *flag.FlagSet) {
+					_ = fs.String("a", "", "a flag")
+				})
 			}
 		})
 	}
@@ -269,16 +179,7 @@ func BenchmarkFeatureComparison(b *testing.B) {
 		args := []string{"prog", "-abcdef"}
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			parser, err := GetOpt(args, "abcdef")
-			if err != nil {
-				b.Fatal(err)
-			}
-			for option, err := range parser.Options() {
-				if err != nil {
-					b.Fatal(err)
-				}
-				_ = option
-			}
+			benchParse(b, args, "abcdef", nil)
 		}
 	})
 
@@ -291,16 +192,7 @@ func BenchmarkFeatureComparison(b *testing.B) {
 
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			parser, err := GetOptLong(args, "", longOpts)
-			if err != nil {
-				b.Fatal(err)
-			}
-			for option, err := range parser.Options() {
-				if err != nil {
-					b.Fatal(err)
-				}
-				_ = option
-			}
+			benchParse(b, args, "", longOpts)
 		}
 	})
 
@@ -313,16 +205,7 @@ func BenchmarkFeatureComparison(b *testing.B) {
 
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			parser, err := GetOptLong(args, "W;", longOpts)
-			if err != nil {
-				b.Fatal(err)
-			}
-			for option, err := range parser.Options() {
-				if err != nil {
-					b.Fatal(err)
-				}
-				_ = option
-			}
+			benchParse(b, args, "W;", longOpts)
 		}
 	})
 }
